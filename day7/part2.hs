d2abdbec7335759d413bba7e691e6cb8e4c9de84
Hamish Tomlinson <hamish.tomlinson@eng.ox.ac.uk>
2021-12-07 10:21:48 +0000

    faster by starting from median

--- day7/part2.hs
+++ day7/part2.hs
@@ -1,15 +1,33 @@
+import Data.List (sort)
+
 main = do
     raw_crab_pos <- readFile "input.txt"
     let crab_pos = map read $ wordsWhen (==',') raw_crab_pos :: [Int]
     let min_pos = minimum crab_pos
-        max_pos = maximum crab_pos in
-            print $ (minimum . map (calcFuelTotal crab_pos)) [min_pos..max_pos]
+        max_pos = maximum crab_pos
+        med_pos = median crab_pos
+        max_int = 2147483647
+        best_low = searchFuel (-1) max_int med_pos min_pos crab_pos
+        best_high = searchFuel 1 max_int med_pos max_pos crab_pos
+            in print $ minimum [best_low, best_high]
+
+searchFuel :: Int -> Int -> Int -> Int -> [Int] -> Int
+searchFuel increment curr_best curr_pos limit crab_pos
+    | (curr_pos == limit) || (fuel > curr_best) = curr_best
+    | otherwise = searchFuel increment fuel (curr_pos + increment) limit crab_pos
+    where fuel = calcFuelTotal crab_pos curr_pos
 
 calcFuelTotal :: [Int] -> Int -> Int
 calcFuelTotal crab_pos align_pos = (sum . map (\x -> calcFuel (x - align_pos))) crab_pos
 
 calcFuel :: Int -> Int
 calcFuel x = let y = abs x in sum [0..y]
+
+median :: [Int] -> Int
+median x = 
+    let l = length x
+        mid = div l 2
+        in sort x !! mid
 
 wordsWhen :: (Char -> Bool) -> String -> [String]
 wordsWhen p s =  case dropWhile p s of
